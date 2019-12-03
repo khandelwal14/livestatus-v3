f6d6fd1841d64dd4855b76f8264e6048e29b0b75
khandelwal14 <khandelwal.sumit14@gmail.com>
2019-12-02 22:12:00 -0800

    updated code

--- src/controllers/Status.js
+++ src/controllers/Status.js
@@ -3,6 +3,7 @@ var Status = require('../service/StatusService');
 
 module.exports.getLiveStatus = function getLiveStatus (req, res, next) {
 	  var serviceRes = Status.getLiveStatus()
+	  console.log("Ankur...live",serviceRes);
 	  return serviceRes;
     
 };
