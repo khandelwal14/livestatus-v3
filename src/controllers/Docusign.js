52ccea933bc4fa161d9e1f6a39cbe16e05a77cd7
khandelwal14 <khandelwal.sumit14@gmail.com>
2019-12-02 21:48:41 -0800

    updated code

--- src/controllers/Docusign.js
+++ src/controllers/Docusign.js
@@ -16,6 +16,13 @@ module.exports.sendEnv = function sendEnv (req, res, next) {
 	console.log("Inside docusign function");
 	  var serviceRes = DocusignEnv.sendEnv();
 	  //var serviceRes = {"Docusign":true,"liveAsOf":"2019-12-01T20:52:34Z"};
-	  return serviceRes;
-
+	  serviceRes.then(function(res){
+		  console.log("response in controller")
+		  console.log("response in controller.1",res)
+		  return res;
+	  }).catch(function(err){
+		  console.log("error in controller")
+		  console.log("error in controller.1",err)
+		  return err;
+	  })
 };
